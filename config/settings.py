8ebd0db587c6a015180948f7116f56f3491170fe
Ismatov <ismatovafzal10@gmail.com>
2026-05-20 15:26:52 +0500

    fix: ALLOWED_HOSTS configured in settings

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,8 @@ SECRET_KEY =env.str('SECRET_KEY')
 
 DEBUG =env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = env.list('ALLOWED_HOST')
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Application definition
