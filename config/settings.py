6a1ff6a6a7860462691a3a44d9fe45c09e58c6b6
Salmon Qahhorov <salmonbekqahhorov@gmail.com>
2026-05-20 15:16:06 +0500

    update ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY =env.str('SECRET_KEY')
 
 DEBUG =env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 
 # Application definition
