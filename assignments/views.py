e09f1ebec4ab6bd23b47c355bbce8ca74ca99a1a
Salmon Qahhorov <salmonbekqahhorov@gmail.com>
2026-05-20 12:08:49 +0500

    fix: add validation and teacher permissions to views

--- assignments/views.py
+++ assignments/views.py
@@ -1,43 +1,55 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from .models import Assignment, Submission, Grade
 from .forms import SubmissionForm, GradeForm
 
 
 
-# @login_required
+
 def assignment_list(request):
     assignments = Assignment.objects.all()
     return render(request, 'assignments/assignment_list.html', {'assignments': assignments})
 
 
-# @login_required
+
+@login_required
 def submit_assignment(request, pk):
     assignment = get_object_or_404(Assignment, pk=pk)
+    submission = Submission.objects.filter(assignment=assignment, student=request.user).first()
+    
     if request.method == 'POST':
-        form = SubmissionForm(request.POST, request.FILES)
+        form = SubmissionForm(request.POST, request.FILES, instance=submission)
         if form.is_valid():
-            sub = form.save(commit=False)
-            sub.student = request.user
-            sub.assignment = assignment
-            sub.save()
-            return redirect('my_grades')
-    else:
-        form = SubmissionForm()
-    return render(request, 'assignments/submit.html', {'form': form})
-
-# @login_required
-def grade_submission(request, pk):
-    if not request.user.is_teacher:
-        return redirect('home')
-    submission = get_object_or_404(Submission, pk=pk)
-    if request.method == 'POST':
-        form = GradeForm(request.POST)
-        if form.is_valid():
-            grade = form.save(commit=False)
-            grade.submission = submission
-            grade.save()
+            new_sub = form.save(commit=False)
+            new_sub.assignment = assignment
+            new_sub.student = request.user
+            new_sub.save()
             return redirect('assignment_list')
     else:
-        form = GradeForm()
-    return render(request, 'assignments/grade.html', {'form': form})
+        form = SubmissionForm(instance=submission)
+    
+    return render(request, 'assignments/submit.html', {'form': form, 'assignment': assignment})
+
+
+
+@login_required
+def grade_submission(request, pk):
+    if not (request.user.is_staff or request.user.groups.filter(name='Teachers').exists()):
+        raise PermissionDenied
+        
+    submission = get_object_or_404(Submission, pk=pk)
+    grade = getattr(submission, 'grade', None)
+    
+    if request.method == 'POST':
+        form = GradeForm(request.POST, instance=grade)
+        if form.is_valid():
+            new_grade = form.save(commit=False)
+            new_grade.submission = submission
+            new_grade.grader = request.user
+            new_grade.save()
+            return redirect('assignment_list')
+    else:
+        form = GradeForm(instance=grade)
+        
+    return render(request, 'assignments/grade.html', {'form': form, 'submission': submission})
